de0329f780ca64dc92ed544ad5d95ac03286d675
YokeswaranMP <yokes2000.wara@gmail.com>
2020-02-07 12:22:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project_tech_support_start
+ PHP Project
---
+++ tech_support/product_manager/index.php
@@ -0,0 +1,37 @@
+<?php
+require('../model/database.php');
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {?>
+<?php include '../view/header.php'; ?>
+<?php 
+global $db;
+$query = 'SELECT * FROM products ORDER BY name';
+$products = $db->query($query);?>
+<table>
+<tr>
+<th>Code</th>
+<th>Name</th>
+<th>Version</th>
+<th>Release Date</th>
+<th>&nbsp;</th>
+</tr>
+<?php foreach ($products as $product) :?>
+<tr>
+	<td><?php echo $product['productCode']; ?></td>
+	<td><?php echo $product['name']; ?></td>
+	<td><?php echo $product['version']; ?></td>
+	<td><?php echo $product['releaseDate']; ?></td>
+	<td><form action="." method="post">
+		<input type="hidden" name="action" value="delete_product" />
+		<input type="hidden" name="product_code" value="<?php echo $product['productCode']; ?>" />
+		<input type="submit" value="Delete" />
+	</form></td>
+</tr>
+<?php endforeach; ?>
+</table>
+<?php include '../view/footer.php'; ?>
+<?php } ?>  
+
